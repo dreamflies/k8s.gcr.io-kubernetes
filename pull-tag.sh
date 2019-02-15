2b7798663f172f0612025d024292ee7dd75583d8
litaiqing <litaiqing@analysys.com.cn>
2019-02-15 18:54:13 +0800

    修改统一重命名tag shell

--- pull-tag.sh
+++ pull-tag.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-images=(kube-proxy-amd64:v1.9.0 kube-scheduler-amd64:v1.9.0 kube-controller-manager-amd64:v1.9.0 kube-apiserver-amd64:v1.9.0
-etcd-amd64:3.1.10 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.8.3 k8s-dns-sidecar-amd64:1.14.7 k8s-dns-kube-dns-amd64:1.14.7
-k8s-dns-dnsmasq-nanny-amd64:1.14.7)
+#要下载的images镜像
+images=( \ 
+	kube-proxy \
+	kube-scheduler \
+	kube-controller-manager \
+	kube-apiserver \
+    etcd \
+    pause \
+    coredns )
+#统一的命名空间
 namespace=networkfox
 for image in ${images[@]} ; do
   docker $namespace /$image
