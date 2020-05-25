a7d6406cd9a67b77a963be9cb65b4ef509302215
litaiqing <ohbugs@foxmail.com>
2020-05-25 18:02:40 +0800

    Update pull-k8s.sh

--- pull-k8s.sh
+++ pull-k8s.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #要下载的images镜像
 images=( \ 
-    kube-proxy:v1.13.3 \
-    kube-scheduler:v1.13.3 \
-    kube-controller-manager:v1.13.3 \
-    kube-apiserver:v1.13.3 \
-    etcd:3.2.24 \
+    kube-proxy:v1.15.2 \
+    kube-scheduler:v1.15.2 \
+    kube-controller-manager:v1.15.2 \
+    kube-apiserver:v1.15.2 \
+    etcd:3.3.10 \
     pause:3.1 \
-    coredns:1.2.6 )
+    coredns:1.3.1 )
 #统一的命名空间
 namespace=networkfox
 for image in ${images[@]} ; do
